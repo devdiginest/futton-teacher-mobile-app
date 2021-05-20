8fd8239ad217362bf8bb0593a1f340fd682e0580
SUJIN <sujin@applab.qa>
2021-05-20 18:49:41 +0530

    api url change

--- src/config/Constants.js
+++ src/config/Constants.js
@@ -1,3 +1,3 @@
 //export const API_URL = 'https://qleap.co.in/futton-api/api/v1/admin';
-export const BASE_URL = "http://apidev.myfutton.com/"
+export const BASE_URL = "https://apidev.myfutton.com/"
 export const API_URL = `${BASE_URL}api/v1/admin`;
